594a47625a13e5782cc7c96d72f18c936128332f
arthurtrevisan01 <arthur.oliva@escola.pr.gov.br>
2026-02-09 16:10:07 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Service Worker para funcionar offline
+const CACHE_NAME = 'leituraflow-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE).catch(() => {
+        console.log('Alguns arquivos não puderam ser cacheados');
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('/index.html');
+    })
+  );
+});
